2b6e2dac5ba30de029d57c98e2c2f4bb2d444fcd
raptazure <raptazure@foxmail.com>
2020-08-15 21:11:11 +0800

    feat: single-threaded to multithreaded

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,19 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
-use std::{fs, net::TcpStream};
+use std::{fs, net::TcpStream, thread, time::Duration};
+use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established");
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,8 +22,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
